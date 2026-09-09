8c6095c62f1325c94b5bb787a28c821a95461da1
Albiere-Lima <albieregames30@gmail.com>
2026-09-09 20:53:27 -0300

    1.0.0-BETA

---
+++ src/main/java/br/ufpb/dcx/lima/albiere/OF_Web/config/DatabaseSeeder.java
@@ -0,0 +1,29 @@
+package br.ufpb.dcx.lima.albiere.OF_Web.config;
+
+import br.ufpb.dcx.lima.albiere.OF_Web.models.User;
+import br.ufpb.dcx.lima.albiere.OF_Web.repositories.UserRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class DatabaseSeeder {
+
+    @Bean
+    CommandLineRunner initDatabase(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        return args -> {
+            String adminEmail = "admin@ufpb.br";
+
+            if (userRepository.findByEmail(adminEmail).isEmpty()) {
+                User admin = new User();
+                admin.setName("Administrador UFPB");
+                admin.setEmail(adminEmail);
+                admin.setPassword(passwordEncoder.encode("admin123")); // Criptografa com BCrypt
+
+                userRepository.save(admin);
+                System.out.println("Usuário admin criado com sucesso!");
+            }
+        };
+    }
+}
