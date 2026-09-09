a519753323b47bab5da3c711927f8a1376ea7822
Albiere-Lima <albieregames30@gmail.com>
2026-09-09 20:24:07 -0300

    1.0.0-BETA

--- src/main/java/br/ufpb/dcx/lima/albiere/OF_Web/config/CorsConfig.java
+++ src/main/java/br/ufpb/dcx/lima/albiere/OF_Web/config/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins(
-                        "http://localhost:5173",
+                        "http://192.168.0.4:5173",
                         "http://localhost:3000" //link para prodrução 3000 para testes do REACT :P
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "PATCH")
