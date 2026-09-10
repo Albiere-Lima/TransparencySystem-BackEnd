850c54dc11cd583fea7ccf0afde06b915eeb4ed9
Albiere-Lima <albieregames30@gmail.com>
2026-09-10 19:44:13 -0300

    1.0.0-BETA

--- src/main/java/br/ufpb/dcx/lima/albiere/OF_Web/config/SecurityCorsConfig.java
+++ src/main/java/br/ufpb/dcx/lima/albiere/OF_Web/config/SecurityCorsConfig.java
@@ -14,7 +14,12 @@ public class SecurityCorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(List.of("*"));
+        configuration.setAllowedOrigins(List.of(
+                "http://localhost:5173",
+                "http://127.0.0.1:5173",
+                "http://192.168.0.7:5173",
+                "http://192.168.0.4:5173"
+        ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
