8f7ad40aa741dc987f2891f8c388a06c7152078e
Albiere-Lima <albieregames30@gmail.com>
2026-09-10 19:39:32 -0300

    1.0.0-BETA

--- src/main/java/br/ufpb/dcx/lima/albiere/OF_Web/config/SecurityConfig.java
+++ src/main/java/br/ufpb/dcx/lima/albiere/OF_Web/config/SecurityConfig.java
@@ -43,7 +43,7 @@ public class SecurityConfig {
 
                         .requestMatchers(HttpMethod.POST, "/api/expenses").hasAnyAuthority("ROLE_ADMIN", "ADMIN")
                         .requestMatchers(HttpMethod.DELETE, "/api/expenses/**").hasAnyAuthority("ROLE_ADMIN", "ADMIN")
-
+                        .requestMatchers("/api/auth/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
