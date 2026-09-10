b31a910a23892badbe202464e43f5e837771b59f
Albiere-Lima <albieregames30@gmail.com>
2026-09-10 19:44:56 -0300

    1.0.0-BETA

--- src/main/java/br/ufpb/dcx/lima/albiere/OF_Web/controllers/AuthController.java
+++ src/main/java/br/ufpb/dcx/lima/albiere/OF_Web/controllers/AuthController.java
@@ -10,7 +10,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/auth")
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = {"http://localhost:5173", "http://192.168.0.4:5173"}, allowCredentials = "true")
 public class AuthController {
 
     private final AuthService authService;
