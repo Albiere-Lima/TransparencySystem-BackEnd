89af55db59dad41151ae6da87812be9cacf3e511
Albiere-Lima <albieregames30@gmail.com>
2026-09-17 14:24:47 -0300

    1.0.0-BETA

--- src/main/java/br/ufpb/dcx/lima/albiere/OF_Web/config/SecurityCorsConfig.java
+++ src/main/java/br/ufpb/dcx/lima/albiere/OF_Web/config/SecurityCorsConfig.java
@@ -14,7 +14,10 @@ public class SecurityCorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(List.of("http://localhost:5173", "http://broadly-driller-moonwalk.ngrok-free.dev/", "https://broadly-driller-moonwalk.ngrok-free.dev/"));
+        configuration.setAllowedOriginPatterns(List.of(
+                "https://*.ngrok-free.dev",
+                "localhost:5173"
+        ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "PATCH"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
