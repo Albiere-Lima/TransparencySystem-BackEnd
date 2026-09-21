0e14f51a1340af82275d9ac4f3cbff6faf36afca
Albiere-Lima <albieregames30@gmail.com>
2026-09-21 18:06:04 -0300

    1.0.0-BETA

--- src/main/java/br/ufpb/dcx/lima/albiere/OF_Web/repositories/ReceiptRepository.java
+++ src/main/java/br/ufpb/dcx/lima/albiere/OF_Web/repositories/ReceiptRepository.java
@@ -3,8 +3,5 @@ package br.ufpb.dcx.lima.albiere.OF_Web.repositories;
 import br.ufpb.dcx.lima.albiere.OF_Web.models.Receipt;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface ReceiptRepository extends JpaRepository<Receipt, Long> {
-    Receipt findByExpenseId(Long expenseId);
 }
