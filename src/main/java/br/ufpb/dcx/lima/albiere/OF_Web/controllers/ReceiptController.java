0806117bfeffdad83a385cc42a930576fe1eac03
Albiere-Lima <albieregames30@gmail.com>
2026-09-21 18:35:13 -0300

    1.0.0-BETA

--- src/main/java/br/ufpb/dcx/lima/albiere/OF_Web/controllers/ReceiptController.java
+++ src/main/java/br/ufpb/dcx/lima/albiere/OF_Web/controllers/ReceiptController.java
@@ -13,7 +13,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
 
 @RestController
-@RequestMapping("/expenses")
+@RequestMapping("/api/expenses")
 @RequiredArgsConstructor
 public class ReceiptController {
 
